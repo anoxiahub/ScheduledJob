3bc923abc9430d13358c342f8d7640e6f02bff71
lin0yuan <3102691553@qq.com>
2023-11-16 20:17:54 +0800

    初始化项目

---
+++ ScheduledJob-admin/src/main/java/com/ayit/scheduled/job/admin/ScheduledJobAdminApplication.java
@@ -0,0 +1,13 @@
+package com.ayit.scheduled.job.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScheduledJobAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScheduledJobAdminApplication.class, args);
+    }
+
+}
---
+++ ScheduledJob-core/src/main/java/com/ayit/scheduledjobcore/ScheduledJobCoreApplication.java
@@ -0,0 +1,13 @@
+package com.ayit.scheduledjobcore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScheduledJobCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScheduledJobCoreApplication.class, args);
+    }
+
+}
