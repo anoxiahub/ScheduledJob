8df777c2d6038669893c30fe7a0e9320d8b0f409
lin0yuan <3102691553@qq.com>
2023-11-18 19:17:32 +0800

    构造任务调度核心模块

--- ScheduledJob-admin/src/main/java/com/ayit/scheduled/job/admin/core/trigger/TriggerTypeEnum.java
+++ ScheduledJob-admin/src/main/java/com/ayit/scheduled/job/admin/core/trigger/TriggerTypeEnum.java
@@ -3,12 +3,6 @@ package com.ayit.scheduled.job.admin.core.trigger;
 
 import com.ayit.scheduled.job.admin.core.util.I18nUtil;
 
-/**
- * @author:B站UP主陈清风扬，从零带你写框架系列教程的作者，个人微信号：chenqingfengyang。
- * @Description:系列教程目前包括手写Netty，XXL-JOB，Spring，RocketMq，Javac，JVM等课程。
- * @Date:2023/7/12
- * @Description:触发枚举类型，这个其实还是对应着触发器任务的类型。一般也都是cron类型的
- */
 public enum TriggerTypeEnum {
     MANUAL(I18nUtil.getString("jobconf_trigger_type_manual")),
     CRON(I18nUtil.getString("jobconf_trigger_type_cron")),
