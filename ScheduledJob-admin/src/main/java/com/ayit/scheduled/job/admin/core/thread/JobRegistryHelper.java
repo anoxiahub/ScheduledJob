e28aa37e8e9e412b9ceb0e5102457126551a4e97
lin0yuan <3102691553@qq.com>
2023-11-22 16:20:58 +0800

    服务端心跳检测判断逻辑

--- ScheduledJob-admin/src/main/java/com/ayit/scheduled/job/admin/core/thread/JobRegistryHelper.java
+++ ScheduledJob-admin/src/main/java/com/ayit/scheduled/job/admin/core/thread/JobRegistryHelper.java
@@ -1,13 +1,16 @@
 package com.ayit.scheduled.job.admin.core.thread;
 
 import com.ayit.scheduled.job.admin.core.conf.XxlJobAdminConfig;
+import com.ayit.scheduled.job.admin.core.model.XxlJobGroup;
+import com.ayit.scheduled.job.admin.core.model.XxlJobRegistry;
 import com.ayit.scheduled.job.core.biz.model.RegistryParam;
 import com.ayit.scheduled.job.core.biz.model.ReturnT;
+import com.ayit.scheduled.job.core.enums.RegistryConfig;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.util.StringUtils;
 
-import java.util.Date;
+import java.util.*;
 import java.util.concurrent.*;
 
 public class JobRegistryHelper {
@@ -21,7 +24,7 @@ public class JobRegistryHelper {
 
 	private ThreadPoolExecutor registryOrRemoveThreadPool = null;
 
-
+	private Thread registryMonitorThread;
 	private volatile boolean toStop = false;
 
 	public void start(){
@@ -44,6 +47,71 @@ public class JobRegistryHelper {
 						logger.warn(">>>>>>>>>>> xxl-job, registry or remove too fast, match threadpool rejected handler(run now).");
 					}
 				});
+
+		registryMonitorThread = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				while (!toStop) {
+					try {
+						List<XxlJobGroup> groupList = XxlJobAdminConfig.getAdminConfig().getXxlJobGroupDao().findByAddressType(0);
+						if (groupList!=null && !groupList.isEmpty()) {
+							List<Integer> ids = XxlJobAdminConfig.getAdminConfig().getXxlJobRegistryDao().findDead(RegistryConfig.DEAD_TIMEOUT, new Date());
+							if (ids!=null && ids.size()>0) {
+								XxlJobAdminConfig.getAdminConfig().getXxlJobRegistryDao().removeDead(ids);
+							}
+							HashMap<String, List<String>> appAddressMap = new HashMap<String, List<String>>();
+							List<XxlJobRegistry> list = XxlJobAdminConfig.getAdminConfig().getXxlJobRegistryDao().findAll(RegistryConfig.DEAD_TIMEOUT, new Date());
+							if (list != null) {
+								for (XxlJobRegistry item: list) {
+									if (RegistryConfig.RegistType.EXECUTOR.name().equals(item.getRegistryGroup())) {
+										String appname = item.getRegistryKey();
+										List<String> registryList = appAddressMap.get(appname);
+										if (registryList == null) {
+											registryList = new ArrayList<String>();
+										}
+										if (!registryList.contains(item.getRegistryValue())) {
+											registryList.add(item.getRegistryValue());
+										}
+										appAddressMap.put(appname, registryList);
+									}
+								}
+							}
+							for (XxlJobGroup group: groupList) {
+								List<String> registryList = appAddressMap.get(group.getAppname());
+								String addressListStr = null;
+								if (registryList!=null && !registryList.isEmpty()) {
+									Collections.sort(registryList);
+									StringBuilder addressListSB = new StringBuilder();
+									for (String item:registryList) {
+										addressListSB.append(item).append(",");
+									}
+									addressListStr = addressListSB.toString();
+									addressListStr = addressListStr.substring(0, addressListStr.length()-1);
+								}
+								group.setAddressList(addressListStr);
+								group.setUpdateTime(new Date());
+								XxlJobAdminConfig.getAdminConfig().getXxlJobGroupDao().update(group);
+							}
+						}
+					} catch (Exception e) {
+						if (!toStop) {
+							logger.error(">>>>>>>>>>> xxl-job, job registry monitor thread error:{}", e);
+						}
+					}
+					try {
+						TimeUnit.SECONDS.sleep(RegistryConfig.BEAT_TIMEOUT);
+					} catch (InterruptedException e) {
+						if (!toStop) {
+							logger.error(">>>>>>>>>>> xxl-job, job registry monitor thread error:{}", e);
+						}
+					}
+				}
+				logger.info(">>>>>>>>>>> xxl-job, job registry monitor thread stop");
+			}
+		});
+		registryMonitorThread.setDaemon(true);
+		registryMonitorThread.setName("xxl-job, admin JobRegistryMonitorHelper-registryMonitorThread");
+		registryMonitorThread.start();
 	}
 
 
